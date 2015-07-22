8c09ab47309200e1208da7678554a286ad7deb94
Thomas Huybrechts <huybthomas@gmail.com>
2015-07-22 15:03:27 +0200

    Update speaker.c
    Indent correction

--- src/speaker.c
+++ src/speaker.c
@@ -7,7 +7,7 @@ void SpeakerInit(void)
 {
 	//Setup IO
 	PORTC.DIRSET = 0b00000001;
-	PORTC.PIN0CTRL = 0b00010001;	//6: non-invert, 5-3: output conf: pull down, 2-0: input conf: rising edge
+	PORTC.PIN0CTRL = 0b00010001;		//6: non-invert, 5-3: output conf: pull down, 2-0: input conf: rising edge
 	
 	//Setup TC for frequency generation
 	TCC0.CTRLA = 0b00000000;		//3-0: Prescaler: uit
@@ -28,15 +28,15 @@ void SpeakerBeep(int Frequency,int Length)
 {
 	int countF = ((16000000)/(Frequency))-1;
 	int countL = ((16000000)/(1024*(1000/Length)))-1;	//(1/Length)*1000  Conversie ms naar Hz
-	TCC0.PER = countF;									//Top-waarde (periode)
-	TCC0.CCA = countF/2;								//Duty-cycle (pulsbreedte)
-	TCC0.CTRLA = 0b00000001;							//3-0: Prescaler: clk (aan)
-	TCC1.PER = countL;									//Top-waarde
-	TCC1.CTRLA = 0b00000111;							//3-0: Prescaler: clk/1024 (aan)
+	TCC0.PER = countF;					//Top-waarde (periode)
+	TCC0.CCA = countF/2;					//Duty-cycle (pulsbreedte)
+	TCC0.CTRLA = 0b00000001;				//3-0: Prescaler: clk (aan)
+	TCC1.PER = countL;					//Top-waarde
+	TCC1.CTRLA = 0b00000111;				//3-0: Prescaler: clk/1024 (aan)
 	
 	//Lengte bepaald zonder interrupt
 	//_delay_ms(500);
-	//TCC0.CTRLA = 0b00000000;							//3-0: Prescaler: uit
+	//TCC0.CTRLA = 0b00000000;				//3-0: Prescaler: uit
 }
 
 ISR(TCC1_OVF_vect)
