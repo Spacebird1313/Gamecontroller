0ea4ca12065971f618a741e5cbcef170f6a5beaa
Thomas Huybrechts <huybthomas@gmail.com>
2015-07-22 15:04:21 +0200

    Update switch.c
    Indent correction

--- src/switch.c
+++ src/switch.c
@@ -4,7 +4,7 @@
 void SwitchInit(void)
 {
 	PORTD.DIRCLR = 0b11110001;			//Set Input
-	PORTD.PIN0CTRL = 0b01011001;		//6: invert, 5-3: output conf: pull up, 2-0: input conf: rising edge
+	PORTD.PIN0CTRL = 0b01011001;			//6: invert, 5-3: output conf: pull up, 2-0: input conf: rising edge
 	PORTD.PIN4CTRL = 0b01011001;
 	PORTD.PIN5CTRL = 0b01011001;
 	PORTD.PIN6CTRL = 0b01011001;
@@ -18,4 +18,3 @@ char SwitchGet(void)
 	
 	return switchInput | (PORTD.IN & 0x01);		//Voeg B0 toe aan switchInput
 }
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
