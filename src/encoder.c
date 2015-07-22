65139762a55c0af2a96d1e16f405fd0e7a46ee43
Thomas Huybrechts <huybthomas@gmail.com>
2015-07-22 15:00:21 +0200

    Update encoder.c
    Indent correction

--- src/encoder.c
+++ src/encoder.c
@@ -7,18 +7,18 @@
 #include "encoder.h"
 
 volatile char EncoderPosition;			//Volatile globale variabelen: dit voorkomt dat de compiler de code verkeerd optimaliseerd. De compiler merkt dat de interrupt methodes niet worden bereikt tijdens codeuitvoering
-										//en zal de variabele standaard op 0 zetten en houden. Het woord 'volatile' zal de compiler dwingen de code niet te optimaliseren voor die variabelen. Als de interrupt wordt uitgevoerd
-										//zal de waarde van de variabelen worden gewijzigd.
+						//en zal de variabele standaard op 0 zetten en houden. Het woord 'volatile' zal de compiler dwingen de code niet te optimaliseren voor die variabelen. Als de interrupt wordt uitgevoerd
+						//zal de waarde van de variabelen worden gewijzigd.
 
 void EncoderInit(void)
 {
-	PORTC.DIRCLR = 0b00110000;			//Set Input
+	PORTC.DIRCLR = 0b00110000;		//Set Input
 	PORTC.PIN4CTRL = 0b00011000;		//5-3: output conf: pull up, 2-0: input conf: both edges
 	PORTC.PIN5CTRL = 0b00011000;
-	PORTC.INTCTRL = 0b00000101;			//3-2: interrupt 1: low level interrupt, 1-0: interrupt 0: low level interrupt
+	PORTC.INTCTRL = 0b00000101;		//3-2: interrupt 1: low level interrupt, 1-0: interrupt 0: low level interrupt
 	PORTC.INT0MASK = 0b00010000;		//interrupt 0: encoder poort 4
 	//PORTC.INT1MASK = 0b00100000;		//interrupt 1: encoder poort 5 - verdubbeling resolutie
-	PMIC.CTRL = 0b10000001;				//7: Round-robin Scheduling enabled, 1: Low-level interrupt enabled
+	PMIC.CTRL = 0b10000001;			//7: Round-robin Scheduling enabled, 1: Low-level interrupt enabled
 }
 
 int EncoderGetPos(void)
@@ -28,28 +28,28 @@ int EncoderGetPos(void)
 
 ISR(PORTC_INT0_vect)
 {
-	_delay_ms(5);										//wacht 5ms voor uitlezing flank (debounce tijd)
+	_delay_ms(5);							//wacht 5ms voor uitlezing flank (debounce tijd)
 	
-	PORTC.INTFLAGS = 0b00000011;						//Reset interruptflag
+	PORTC.INTFLAGS = 0b00000011;					//Reset interruptflag
 	
-	if((PORTC.IN & 0x10) == 0x10)						//PC4 rising edge
+	if((PORTC.IN & 0x10) == 0x10)					//PC4 rising edge
 	{
-		if((PORTC.IN & 0x20) == 0x20)					//PC5 hoog
+		if((PORTC.IN & 0x20) == 0x20)				//PC5 hoog
 		{
 			EncoderPosition = EncoderPosition - 1;		//Encoder decrementeren
 		}
-		else											//PC5 laag
+		else							//PC5 laag
 		{
 			EncoderPosition = EncoderPosition + 1;		//Encoder incrementeren
 		}
 	}
-	else												//PC4 falling edge
+	else								//PC4 falling edge
 	{
-		if((PORTC.IN & 0x20) == 0x20)					//PC5 hoog
+		if((PORTC.IN & 0x20) == 0x20)				//PC5 hoog
 		{
 			EncoderPosition = EncoderPosition + 1;		//Encoder incrementeren
 		}
-		else											//PC5 laag
+		else							//PC5 laag
 		{
 			EncoderPosition = EncoderPosition - 1;		//Encoder decrementeren
 		}
@@ -58,31 +58,30 @@ ISR(PORTC_INT0_vect)
 
 ISR(PORTC_INT1_vect)
 {
-	_delay_ms(5);										//wacht 5ms voor uitlezing flank (debounce tijd)
+	_delay_ms(5);							//wacht 5ms voor uitlezing flank (debounce tijd)
 	
-	PORTC.INTFLAGS = 0b00000011;						//Reset interruptflag
+	PORTC.INTFLAGS = 0b00000011;					//Reset interruptflag
 	
-	if((PORTC.IN & 0x20) == 0x20)						//PC5 rising edge
+	if((PORTC.IN & 0x20) == 0x20)					//PC5 rising edge
 	{
-		if((PORTC.IN & 0x10) == 0x10)					//PC4 hoog
+		if((PORTC.IN & 0x10) == 0x10)				//PC4 hoog
 		{
 			EncoderPosition = EncoderPosition + 1;		//Encoder incrementeren
 		}
-		else											//PC4 laag
+		else							//PC4 laag
 		{
 			EncoderPosition = EncoderPosition - 1;		//Encoder decrementeren
 		}
 	}
-	else												//PC5 falling edge
+	else								//PC5 falling edge
 	{
-		if((PORTC.IN & 0x10) == 0x10)					//PC4 hoog
+		if((PORTC.IN & 0x10) == 0x10)				//PC4 hoog
 		{
 			EncoderPosition = EncoderPosition - 1;		//Encoder decrementeren
 		}
-		else											//PC4 laag
+		else							//PC4 laag
 		{
 			EncoderPosition = EncoderPosition + 1;		//Encoder incrementeren
 		}
 	}
 }
-
